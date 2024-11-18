d88aeab4b6fee95ec1d21ac0a299d429c701b0ae
Marat Gaffarov <gaffarov142004@gmail.com>
2024-11-18 23:02:31 +0300

    Revert "done HW5"
    This reverts commit a57c2051fe8cf71f101a238d52b41f2db358e197.

--- Dotnet.Homeworks.MainProject/Services/RegistrationService.cs
+++ Dotnet.Homeworks.MainProject/Services/RegistrationService.cs
@@ -7,12 +7,17 @@ public class RegistrationService : IRegistrationService
 {
     private readonly ICommunicationService _communicationService;
 
-    public RegistrationService(ICommunicationService communicationService) => _communicationService = communicationService;
+    public RegistrationService(ICommunicationService communicationService)
+    {
+        _communicationService = communicationService;
+    }
 
     public async Task RegisterAsync(RegisterUserDto userDto)
     {
+        // pretending we have some complex logic here
         await Task.Delay(100);
-
+        
+        // publish message to a queue
         await _communicationService.SendEmailAsync(new SendEmail("", "", "", ""));
     }
 }
